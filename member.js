b1c0d65a3d9d2c55e718856623af8a5f9049a284
kbordac-monalee <131376610+kbordac-monalee@users.noreply.github.com>
2023-12-07 17:26:38 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,14 @@
 function stillsMember() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/stills/member.html',
-        controller: 'StillsMemberCtrl',
-        controllerAs: 'vm',
-        bindToController: true,
-        scope: {
-        stills: '='
-        }
-    };
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    if (memberValue == "yes") {
+        document.getElementById("memberName").style.display = "block";
+        document.getElementById("memberName").style.visibility = "visible";
+        document.getElementById("memberName").required = true;
     }
+    else {
+        document.getElementById("memberName").style.display = "none";
+        document.getElementById("memberName").style.visibility = "hidden";
+        document.getElementById("memberName").required = false;
+    }
+}
